2a4cf97777bf17418d88a0fe78923c7e2fc5310f
clotton <clotton@adobe.com>
2025-03-27 15:10:21 -0400

    Merge remote-tracking branch 'origin/main'

--- slack-admin/config.js
+++ slack-admin/config.js
@@ -1,2 +1,2 @@
-const API_ENDPOINT = window.location.href.includes('localhost') ? 'http://localhost:8787' : 'https://eds-channels-tracker-worker.chrislotton.workers.dev/';
+const API_ENDPOINT = window.location.href.includes('localhost') ? 'http://localhost:8787' : 'https://eds-channels-tracker-worker.chrislotton.workers.dev/slack/channels';
 export default API_ENDPOINT;
