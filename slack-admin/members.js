a1bc6a53bad7ad3b29cb48c6433a0c61de3cafef
kovac <kovac@adobe.com>
2025-04-09 18:54:57 -0400

    Fix linting errors

--- slack-admin/members.js
+++ slack-admin/members.js
@@ -1,20 +1,22 @@
 // members.js
 import { getUserInfo } from './api.js';
 
-
+// eslint-disable-next-line import/prefer-default-export
 export const countMembers = async (members) => {
-  let adobeMembers = [];
-  let nonAdobeMembers = [];
+  const adobeMembers = [];
+  const nonAdobeMembers = [];
 
+  // eslint-disable-next-line no-restricted-syntax
   for (const userId of members) {
+    // eslint-disable-next-line no-await-in-loop
     const userJson = await getUserInfo(userId);
     const email = userJson.user?.profile?.email;
-    if (email && email.endsWith("@adobe.com")) {
+    if (email && email.endsWith('@adobe.com')) {
       adobeMembers.push(userJson.user.real_name);
     } else {
       let name = userJson.user?.real_name || userJson.user?.name;
       if (userJson.user.is_bot || userJson.user.is_app_user) {
-        name = '' + name + ' (bot)';
+        name = `${name} (bot)`;
       }
       nonAdobeMembers.push(name);
     }
