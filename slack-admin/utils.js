e6fb1169a3b461faeb7c5268ec200fd04f2c9586
clotton <clotton@adobe.com>
2025-04-16 14:52:40 -0400

    Cleanup utils code

--- slack-admin/utils.js
+++ slack-admin/utils.js
@@ -1,14 +1,27 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 /* eslint-disable no-underscore-dangle */
 
 export const sortTable = (data, key, direction) => {
-  const getComparableValue = (val) =>
-    key === 'lstMsgDt'
-      ? val === 'No Messages' ? null : new Date(val)
-      : val;
+  const getComparableValue = (val) => {
+    if (key !== 'lstMsgDt') return val;
+
+    const isNoMessages = val === 'No Messages';
+    return isNoMessages ? null : new Date(val);
+  };
 
   return [...data].sort((a, b) => {
-    let valueA = getComparableValue(a[key]);
-    let valueB = getComparableValue(b[key]);
+    const valueA = getComparableValue(a[key]);
+    const valueB = getComparableValue(b[key]);
 
     if (valueA === null) return direction === 'asc' ? -1 : 1;
     if (valueB === null) return direction === 'asc' ? 1 : -1;
@@ -27,8 +40,7 @@ export const sortTable = (data, key, direction) => {
   });
 };
 
-export const alphaSort = (a, b) =>
-  a.localeCompare(b, undefined, { sensitivity: 'base' });
+export const alphaSort = (a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' });
 
 const positionModal = (modal, cell) => {
   const { top, left } = cell.getBoundingClientRect();
@@ -44,7 +56,7 @@ const hideModal = (modal) => {
       modal.innerHTML = '<span class="spinner"></span>';
       modal.style.display = 'none';
     },
-    { once: true }
+    { once: true },
   );
 };
 
