7c8dd1a189bcac9a7164b805e5fe6e79bb98bfa3
kovac <kovac@adobe.com>
2025-04-28 13:37:27 -0400

    fix: Description

--- teams-admin/main.js
+++ teams-admin/main.js
@@ -75,7 +75,7 @@ const renderTable = (teams) => {
       nameCell.textContent = team.displayName;
     }
 
-    const descriptionText = decodeHTML(team?.description.value || '');
+    const descriptionText = decodeHTML(team.description || '');
     const descriptionCell = createCell(descriptionText);
     const createdCell = createCell(team.created || '', 'stat-column created');
     const channelCountCell = createCell(team.channelCount || '', 'stat-column channels-count');
