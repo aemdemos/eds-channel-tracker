9404f718c9c713a0eee98b63620b62e4965f819b
clotton <clotton@adobe.com>
2025-06-10 12:29:15 -0400

    turnstile

--- teams-admin/main.js
+++ teams-admin/main.js
@@ -39,7 +39,7 @@ let userProfile;
 const isLocalhost = ['localhost', '127.0.0.1'].includes(window.location.hostname);
 
 if (!isLocalhost) {
-  window.requestIdleCallback = () => {
+  window.onload = () => {
     onTurnstileLoad();
   };
 }
